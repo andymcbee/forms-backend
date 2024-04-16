1fb24023fd705fdebe2332fb9e8895ca49f1433a
Andrew <armcburn@gmail.com>
2024-04-16 10:51:59 -0400

    fix zod validation for form name field name

--- api/src/models/api/forms/submitFormRequestBodySchema.ts
+++ api/src/models/api/forms/submitFormRequestBodySchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const submitFormRequestBodySchema = z.object({
-  "form-name": z.string().min(1).max(50),
+  formName: z.string().min(1).max(50),
   data: z.record(z.unknown())
 });
